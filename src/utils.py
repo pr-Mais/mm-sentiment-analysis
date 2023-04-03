28f54bc4de8a621d13d9599e7a8a589620d60147
Mais <mais.alheraki@gmail.com>
2023-04-03 04:30:23 +0300

    remove comment

--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,11 @@ import datetime
 import torch
 import numpy as np
 
-# Function to calculate the accuracy of our predictions vs labels
-
 
 def flat_accuracy(preds, labels):
+    '''
+    Calculate the accuracy of predictions vs labels.
+    '''
     pred_flat = np.argmax(preds, axis=1).flatten()
     labels_flat = labels.flatten()
     return np.sum(pred_flat == labels_flat) / len(labels_flat)
